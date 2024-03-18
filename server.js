59a13220109dfa814fd52c68e3ef9ffed73a5696
ferenc_koler <ferenckoler@outlook.com>
2024-03-18 11:27:56 +0100

    Temperature Example

--- server.js
+++ server.js
@@ -19,16 +19,36 @@ app.get('/users', async (req, res) => {
     //     { id: 3, name: 'Shannon Jackson' },
     // ];
 
-    const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+    setTimeout(async () => {
+        const limit = +req.query.limit || 10;
 
-    const users = await response.json();
+        const response = await fetch(
+            `https://jsonplaceholder.typicode.com/users?_limit=${limit}`
+        );
 
-    res.send(`
+        const users = await response.json();
+
+        res.send(`
     <h1 class="text-2xl font-bold my-4">Users</h1>
     <ul>
       ${users.map((user) => `<li>${user.name}</li>`).join('')}
     </ul>
-  `);
+    `);
+    }, 500);
+});
+
+// Handle POST request for temp conversion
+app.post('/convert', (req, res) => {
+    setTimeout(() => {
+        const fahrenheit = +req.body.fahrenheit;
+        const celsius = ((fahrenheit - 32) * (5 / 9)).toFixed(2);
+
+        res.send(`
+      <p>
+        ${fahrenheit} degrees Farenheit is equal to ${celsius} degrees Celsius
+      </p>
+    `);
+    }, 500);
 });
 
 // Start the server
