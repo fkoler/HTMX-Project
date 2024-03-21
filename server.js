5bb45c74c10f08d6f2250bf8c6acbaad97509b95
ferenc_koler <ferenckoler@outlook.com>
2024-03-21 11:40:56 +0100

    Validation Example

--- server.js
+++ server.js
@@ -142,6 +142,64 @@ app.post('/search/api', async (req, res) => {
     }, 500);
 });
 
+// Handle POST request for email validation
+app.post('/contact/email', (req, res) => {
+    const submittedEmail = req.body.email;
+    const emailRegex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+
+    const isValid = {
+        message: 'That email is valid',
+        class: 'text-green-700',
+    };
+
+    const isInvalid = {
+        message: 'Please enter a valid email address',
+        class: 'text-red-700',
+    };
+
+    if (!emailRegex.test(submittedEmail)) {
+        return res.send(
+            `
+      <div class="mb-4" hx-target="this" hx-swap="outerHTML">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="email"
+        >Email Address</label
+      >
+      <input
+        name="email"
+        hx-post="/contact/email"
+        class="border rounded-lg py-2 px-3 w-full focus:outline-none focus:border-blue-500"
+        type="email"
+        id="email"
+        value="${submittedEmail}"
+        required
+      />
+      <div class="${isInvalid.class}">${isInvalid.message}</div>
+    </div>
+      `
+        );
+    } else {
+        return res.send(
+            `
+      <div class="mb-4" hx-target="this" hx-swap="outerHTML">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="email"
+        >Email Address</label
+      >
+      <input
+        name="email"
+        hx-post="/contact/email"
+        class="border rounded-lg py-2 px-3 w-full focus:outline-none focus:border-blue-500"
+        type="email"
+        id="email"
+        value="${submittedEmail}"
+        required
+      />
+      <div class="${isValid.class}">${isValid.message}</div>
+    </div>
+      `
+        );
+    }
+});
+
 // Start the server
 app.listen(3000, () => {
     console.log('Server listening on port 3000...');
